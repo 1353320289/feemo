cdbc5b38acbc05bf57ae53c48afe0fc6b5d34235
1353320289 <134351927+1353320289@users.noreply.github.com>
2024-09-14 08:55:09 +0800

    Update tcpx.sh

--- tcpx.sh
+++ tcpx.sh
@@ -1256,16 +1256,16 @@ net.ipv6.conf.default.accept_ra = 2" >>/etc/sysctl.d/99-sysctl.conf
 start_menu() {
   clear
   echo && echo -e " Feemo 方生--加速器 ${Red_font_prefix}[v${sh_ver}] 不卸内核${Font_color_suffix} 
- 仅供学习交流,严禁用于商业用途--Feemo${Font_color_suffix}
- ${Green_font_prefix}1.${Font_color_suffix} 安装 BBR原版内核
- ${Green_font_prefix}2.${Font_color_suffix} 安装 BBRplus版内核		
- ${Green_font_prefix}3.${Font_color_suffix} 安装 Lotserver(锐速)内核
- ${Green_font_prefix}4.${Font_color_suffix} 安装 BBRplus新版内核
- ${Green_font_prefix}5.${Font_color_suffix} 使用BBR+FQ加速
- ${Green_font_prefix}6.${Font_color_suffix} 使用BBR+FQ_PIE加速 
- ${Green_font_prefix}7.${Font_color_suffix} 使用BBR+CAKE加速
- ${Green_font_prefix}8.${Font_color_suffix} 使用BBR2+FQ加速
- ${Green_font_prefix}9.${Font_color_suffix} 使用BBR2+FQ_PIE加速
+ ${Red_font_prefix}仅供学习交流,严禁用于商业用途--Feemo${Font_color_suffix}
+ ${Green_font_prefix}1.${Font_color_suffix}  安装 BBR原版内核
+ ${Green_font_prefix}2.${Font_color_suffix}  安装 BBRplus版内核		
+ ${Green_font_prefix}3.${Font_color_suffix}  安装 Lotserver(锐速)内核
+ ${Green_font_prefix}4.${Font_color_suffix}  安装 BBRplus新版内核
+ ${Green_font_prefix}5.${Font_color_suffix}  使用BBR+FQ加速
+ ${Green_font_prefix}6.${Font_color_suffix}  使用BBR+FQ_PIE加速 
+ ${Green_font_prefix}7.${Font_color_suffix}  使用BBR+CAKE加速
+ ${Green_font_prefix}8.${Font_color_suffix}  使用BBR2+FQ加速
+ ${Green_font_prefix}9.${Font_color_suffix}  使用BBR2+FQ_PIE加速
  ${Green_font_prefix}10.${Font_color_suffix} 使用BBR2+CAKE加速	 		
  ${Green_font_prefix}11.${Font_color_suffix} 使用BBRplus+FQ版加速
  ${Green_font_prefix}12.${Font_color_suffix} 使用Lotserver(锐速)加速
@@ -1273,6 +1273,7 @@ start_menu() {
  ${Green_font_prefix}14.${Font_color_suffix} 删除保留指定内核
  ${Green_font_prefix}15.${Font_color_suffix} 卸载全部加速
  ${Green_font_prefix}99.${Font_color_suffix} 退出脚本
+ ${Red_font_prefix}仅供学习交流,严禁用于商业用途--Feemo${Font_color_suffix}
 ————————————————————————————————————————————————————————————————" &&
     check_status
   get_system_info
