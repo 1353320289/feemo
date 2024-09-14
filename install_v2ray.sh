547eb4baf8dacd01235c56a428c3621f733c0bae
1353320289 <134351927+1353320289@users.noreply.github.com>
2024-09-14 14:17:29 +0800

    Update install_v2ray.sh

--- install_v2ray.sh
+++ install_v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 自己用的V2--Feemo 方生
-# Modify by ifeng<https://www.hicairo.com>
+# by FLL
 
 RED="\033[31m"      # Error message
 GREEN="\033[32m"    # Success message
@@ -1831,11 +1831,11 @@ menu() {
     clear
     echo "#############################################################"
     echo -e "#                   ${RED}Feemo--VPS协议安装${PLAIN}                 #"
-    echo -e "# ${GREEN}作者${PLAIN}: FLL                                  #"
-    echo -e "# ${GREEN}维护${PLAIN}: FLL                                  #"
-    echo -e "# ${GREEN}注意${PLAIN}: 仅供学习交流 严禁用于商业用途--Feemo    #"
-    echo -e "# ${GREEN}备注${PLAIN}: Feemo内部使用                         #"                    
-    echo -e "# 多${GREEN}协议${PLAIN}选择！！！                           #"                      
+    echo -e "# ${GREEN}作者${PLAIN}: FLL                                         #"
+    echo -e "# ${GREEN}维护${PLAIN}: FLL                                        #"
+    echo -e "# ${GREEN}注意${PLAIN}: 仅供学习交流 严禁用于商业用途--Feemo         #"
+    echo -e "# ${GREEN}备注${PLAIN}: Feemo内部使用                              #"                    
+    echo -e "# 多${GREEN}协议${PLAIN}选择！！！                                 #"                      
     echo "#############################################################"
 
     echo -e "  ${GREEN}1.${PLAIN}   安装V2ray-VMESS"
